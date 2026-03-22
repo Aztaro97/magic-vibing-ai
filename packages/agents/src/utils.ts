ef23a6aa460511881c8700015bf0a2354e3bee81
Taro <aztaro97@gmail.com>
2026-03-22 21:07:13 +0400

    chore: Remove the `agents` package and update `pnpm-lock.yaml` and `langgraph.json`

--- packages/agents/src/utils.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import { Sandbox } from "@e2b/code-interpreter";
-
-export async function getSandbox(sandboxId: string) {
-  const sandbox = await Sandbox.connect(sandboxId);
-  await sandbox.setTimeout(5 * 60_000); // 5 minutes
-  return sandbox;
-}
